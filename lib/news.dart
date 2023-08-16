1c139ed17099100f4dac354c1fa084a5bee05acf
Shreyash Pandey <shreyash.edu@gmail.com>
2023-08-16 16:30:50 +0530

    fixed bugs

--- lib/news.dart
+++ lib/news.dart
@@ -1,13 +1,15 @@
 import 'dart:convert';
 
-import 'package:news_app/models/article.dart';
 import 'package:http/http.dart' as http;
+
+import 'package:news_app/models/article.dart';
+import 'package:news_app/secrets.dart';
 
 class News {
   List<ArticleModel> articles = [];
   Future<void> getNews() async {
     var Url = Uri.parse(
-        'https://newsapi.org/v2/top-headlines?country=in&apiKey=230cd17d6c404e15b798b9cd9c19c3c7');
+        'https://newsapi.org/v2/top-headlines?country=in&apiKey=${Secret.apiKey}');
     var response = await http.get(Url);
     var jsonData = jsonDecode(response.body);
     if (jsonData['status'] == 'ok') {
@@ -31,7 +33,7 @@ class CategoryNews {
   List<ArticleModel> articles = [];
   Future<void> getNews(String category) async {
     var Url = Uri.parse(
-        'https://newsapi.org/v2/top-headlines?category=$category&country=in&apiKey=230cd17d6c404e15b798b9cd9c19c3c7');
+        'https://newsapi.org/v2/top-headlines?category=$category&country=in&apiKey=${Secret.apiKey}');
     var response = await http.get(Url);
     var jsonData = jsonDecode(response.body);
     if (jsonData['status'] == 'ok') {
